5f5389151a6925eef00fbe6e607df08812c81f46
mike o'brien <mpobrien005@gmail.com>
2015-05-21 15:47:25 -0400

    initial commit

---
+++ perf.go
@@ -0,0 +1,73 @@
+package git
+
+import (
+	"fmt"
+	"github.com/evergreen-ci/evergreen/plugin"
+	"github.com/evergreen-ci/evergreen/util"
+	"github.com/mitchellh/mapstructure"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+)
+
+func init() {
+	plugin.Publish(&PerfPlugin{})
+}
+
+var includes = []template.HTML{
+	`<script src="//cdnjs.cloudflare.com/ajax/libs/d3/3.5.3/d3.min.js"></script>`,
+	`<script src="//cdnjs.cloudflare.com/ajax/libs/rickshaw/1.5.1/rickshaw.js"></script>`,
+	`<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/rickshaw/1.5.1/rickshaw.css">`,
+	`<script type="text/javascript" src="/plugin/perf/static/js/perf.js"></script>`,
+}
+
+// GitPlugin handles fetching source code and applying patches
+// using the git version control system.
+type PerfPlugin struct {
+	Projects []string `yaml:"string"`
+}
+
+// Name implements Plugin Interface.
+func (pp *PerfPlugin) Name() string {
+	return "perf"
+}
+
+func (pp *PerfPlugin) GetAPIHandler() http.Handler { return nil }
+func (pp *PerfPlugin) GetUIHandler() http.Handler  { return nil }
+func (pp *PerfPlugin) Configure(params map[string]interface{}) error {
+	err := mapstructure.Decode(params, pp)
+	if err != nil {
+		return fmt.Errorf("error decoding %v params: %v", pp.Name(), err)
+	}
+	return nil
+}
+
+func (pp *PerfPlugin) GetPanelConfig() (*plugin.PanelConfig, error) {
+	root := plugin.StaticWebRootFromSourceFile()
+	panelHTML, err := ioutil.ReadFile(root + "/task_perf_data.html")
+	if err != nil {
+		return nil, fmt.Errorf("Can't load panel html file: %v", err)
+	}
+
+	return &plugin.PanelConfig{
+		StaticRoot: plugin.StaticWebRootFromSourceFile(),
+		Panels: []plugin.UIPanel{
+			{
+				Includes:  includes,
+				Page:      plugin.TaskPage,
+				Position:  plugin.PageCenter,
+				PanelHTML: template.HTML(panelHTML),
+				DataFunc: func(context plugin.UIContext) (interface{}, error) {
+					return struct {
+						Enabled bool `json:"enabled"`
+					}{util.SliceContains(pp.Projects, context.ProjectRef.Identifier)}, nil
+				},
+			},
+		},
+	}, nil
+	return nil, nil
+}
+
+func (pp *PerfPlugin) NewCommand(cmdName string) (plugin.Command, error) {
+	return nil, &plugin.ErrUnknownCommand{cmdName}
+}
---
+++ static/js/perf.js
@@ -0,0 +1,171 @@
+function PerfController($scope, $window, $http){
+  var numericFilter = function(x){
+    return !isNaN(parseInt(x))
+  }
+  $scope.conf = $window.plugins["perf"]
+  $scope.task = $window.task_data
+  $scope.perftab=1
+  $scope.getThreadKeys = function(r){
+    var keys = _.uniq(_.filter(_.flatten(_.map(r, function(x){ return _.keys(x.results) }), true), numericFilter))
+    return keys
+  }
+
+  $scope.getMax = function(r){
+    return _.max(_.filter(_.pluck(_.values(r), 'ops_per_sec'), numericFilter))
+
+  }
+  if($scope.conf.enabled){
+
+    // Populate the graph and table for this task
+    $http.get("/plugin/json/task/" + $scope.task.id + "/")
+      .success(function(d){
+        $scope.sample = d
+        var w = 700
+        var bw = 3
+        var h = 100
+        setTimeout(function(){
+          for(var i=0;i<$scope.sample.data.results.length;i++){
+            testname = $scope.sample.data.results[i].name
+            series = $scope.sample.data.results[i].results
+            var keys = _.filter(_.keys($scope.sample.data.results[i].results), numericFilter)
+            var threadsVsOps = []
+            for(var j=0;j<keys.length;j++){
+              threadsVsOps.push({x:j, y:$scope.sample.data.results[i].results[keys[j]].ops_per_sec})
+            }
+
+            var graph = new Rickshaw.Graph( {
+              element: document.querySelector("#chart-" + $scope.task.id + "-" + i), 
+              width: 150, 
+              height: 50, 
+              renderer:"bar",
+              series: [{
+                color: 'lightblue',
+                data: threadsVsOps,
+              }]
+            })
+
+            var yAxis = new Rickshaw.Graph.Axis.Y({ graph: graph, ticks:2 });
+            var x_axis = new Rickshaw.Graph.Axis.X({ 
+              graph: graph, 
+              orientation:"top",
+              tickValues:_.map(keys, function(k){return parseInt(k)-.5}), 
+              tickFormat:function(t){return Math.floor(t)+1}
+            })
+            graph.render();
+          }},0)
+      })
+
+    function generateSummary(sample){
+      var tout = $('<table></table>')
+      for(k in sample){
+        var row = $('<tr>')
+        row.append($('<th></th>').text(k))
+        row.append($('<td></td>').text(sample[k]))
+        tout.append(row)
+      }
+      tout.append($('<tr></tr>')
+      return tout
+    }  
+
+    var testSeriesByName = {}
+    // Populate the trend data
+    $http.get("/plugin/json/history/" + $scope.task.id + "/perf")
+      .success(function(d){
+        console.log(d.length)
+        console.log(d)
+        for (var i = 0; i < d.length; i++) {
+          for (var j = 0; j < d[i].data.results.length; j++) {
+            var name = d[i].data.results[j].name
+            if (!(name in testSeriesByName)) {
+              testSeriesByName[name] = []
+            }
+            var rec = d[i].data.results[j]
+            var maxops = _.max(_.pluck(_.filter(_.values(rec.results), function(x){return typeof(x)=="object"}), "ops_per_sec"))
+            testSeriesByName[name].push({
+              task_id: d[i].task_id,
+              "ops_per_sec": maxops,
+              order: d[i].order
+            })
+          }
+        }
+
+        for(key in testSeriesByName){
+          testSeriesByName[key] = _.sortBy(testSeriesByName[key], 'order')
+        }
+    
+        setTimeout(function(){
+          for(key in testSeriesByName){
+            console.log(testSeriesByName[key])
+             var w = 400
+             var bw = 6
+             var h = 100
+             var svg = d3.select("#perf-trendchart-" + $scope.task.id)
+                  .append("svg")
+                  .attr('class',"series")
+                  .attr("width", 800)
+                  .attr("height", h);
+             var series = testSeriesByName[key]
+             var ops = _.pluck(series, 'ops_per_sec')
+
+            var y = d3.scale.linear()
+              .domain([0, d3.max(ops)])
+              .range([h, 0]);
+            var x = d3.scale.linear()
+              .domain([0, ops.length-1])
+              .range([0, w]);
+
+
+             svg.selectAll('rect')
+               .data(series)
+                 .enter()
+               .append('rect')
+                 .attr('stroke', function(d){
+                    if(d.task_id == $scope.task.id){
+                      return 'green'
+                    }
+                    return '#ccc'
+                 })
+                 .attr('fill', '#eee')
+                 .attr('x', function(d,i){return x(i)})
+                 .attr('y', function(d){return y(d.ops_per_sec)})
+                 .attr('width',bw)
+                 .attr('height', function(d){return y(0) - y(d.ops_per_sec)})//function(d){(d.ops_per_sec/(maxOps-minOps))*100})
+                 .on('mouseover', function(d) {
+                    d3.select(this).attr('fill',"lightblue")
+                    $('#perf-display-' + $scope.task.id).html(generateSummary(d))
+                 })
+                 .on('mouseout', function(d) {
+                    d3.select(this).attr('fill',"#eee")
+                    $('#perf-display-' + $scope.task.id).html(generateSummary(d))
+                 })
+
+                 var avgOpsPerSec = d3.mean(ops)
+                 var avgLine = d3.svg.line()
+                     .x(function(d, i){return x(i)})
+                     .y(function(d){ return y(avgOpsPerSec)})
+
+                 svg.append("line")
+                    .attr("stroke","#6666FF")
+                    .attr("stroke-width","1")
+                    .attr("stroke-dasharray","5,5")
+                    .attr("class", "mean-line")
+                    .attr( {x1:x(0), x2:x(w), y1:y(avgOpsPerSec), y2:y(avgOpsPerSec)})
+
+
+            var padding=30
+             var yAxis = d3.svg.axis()
+                  .scale(y)
+                  .orient("left")
+                  .ticks(5);
+             svg.append("g")
+                .attr("class", "axis")
+                //.attr("transform", "translate(" + padding + ",0)")
+                .call(yAxis);
+
+            break
+          }
+        })
+      })
+  }
+}
+
